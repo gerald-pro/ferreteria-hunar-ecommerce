dbc206581f42ef43cd706f48a45f146854f6a387
Gerald <avaloss.gerald@gmail.com>
2024-11-16 21:31:18 -0400

    first commit

---
+++ database/seeders/PermissionSeeder.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+
+class PermissionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run()
+    {
+        // Crear permisos
+        $permissions = [
+            'user.index',
+            'user.edit',
+            'category.index',
+            'category.create',
+            'category.edit',
+            'category.delete',
+            'product.index',
+            'product.create',
+            'product.edit',
+            'product.delete',
+            'order.index',
+            'order.index.own',
+            'order.edit',
+            'payment.index',
+            'payment.index.own',
+            'statistic.index',
+            'role.index',
+            'role.create',
+            'role.edit',
+            'role.delete',
+            'page.index',
+            'sidebar.index',
+            'sidebar.create',
+            'sidebar.edit',
+            'sidebar.delete',
+            'searcher',
+            'inventory.index',
+            'inventory.create'
+        ];
+
+        foreach ($permissions as $permission) {
+            Permission::create(['name' => $permission]);
+        }
+
+        $rolesAndPermissions = [
+            'cliente' => [
+                'order.index.own',
+                'payment.index.own',
+            ],
+            'gerente comercial' => [
+                'user.index',
+                'user.edit',
+                'category.index',
+                'category.create',
+                'category.edit',
+                'category.delete',
+                'product.index',
+                'product.create',
+                'product.edit',
+                'product.delete',
+                'order.index',
+                'order.edit',
+                'payment.index',
+                'statistic.index',
+                'role.index',
+                'role.create',
+                'role.edit',
+                'role.delete',
+                'page.index',
+                'sidebar.index',
+                'sidebar.create',
+                'sidebar.edit',
+                'sidebar.delete',
+                'searcher',
+                'inventory.index',
+                'inventory.create'
+            ],
+            'asistente de gerencia' => [
+                'user.index',
+                'category.index',
+                'category.create',
+                'category.edit',
+                'category.delete',
+                'product.create',
+                'product.edit',
+                'page.index',
+                'sidebar.index',
+                'sidebar.create',
+                'sidebar.edit',
+                'sidebar.delete',
+                'searcher',
+            ],
+            'jefe de inventario' => [
+                'product.index',
+                'order.index',
+                'order.edit',
+                'inventory.index',
+                'inventory.create'
+            ],
+        ];
+
+        foreach ($rolesAndPermissions as $roleName => $permissions) {
+            $role = Role::create(['name' => $roleName]);
+            $role->givePermissionTo($permissions);
+        }
+    }
+}
